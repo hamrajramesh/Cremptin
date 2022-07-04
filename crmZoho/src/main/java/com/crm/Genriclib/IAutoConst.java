651809c462ae16b1c1e61899b9b8c1e393a3aae3
Admin <Admin@DESKTOP-TAOG1EQ>
2022-07-04 14:48:13 +0530

    pathcommit

--- crmZoho/src/main/java/com/crm/Genriclib/IAutoConst.java
+++ crmZoho/src/main/java/com/crm/Genriclib/IAutoConst.java
@@ -2,13 +2,13 @@ package com.crm.Genriclib;
 
 public interface IAutoConst {
 	String CHROME_KEY="webdriver.chrome.driver";
-	String CHROME_VALUE="C:\\Users\\Admin\\eclipse-workspace\\.metadata\\crmZoho\\src\\main\\resources\\drivers\\chromedriver.exe";
+	String CHROME_VALUE="C:\\Users\\Admin\\git\\repository\\crmZoho\\src\\main\\resources\\drivers\\chromedriver.exe";
 	
 	String GECKO_KEY="webdriver.gecko.driver";
 	String GECKO_VALUE="./drivers/geckodriver.exe";
 	
-	String PROP_PATH="C:\\Users\\Admin\\eclipse-workspace\\.metadata\\crmZoho\\src\\test\\resources\\dataRepo\\config.properties";
-	String EXCEL_PATH="C:\\Users\\Admin\\eclipse-workspace\\.metadata\\crmZoho\\src\\test\\resources\\dataRepo\\inputData.xlsx";
+	String PROP_PATH="C:\\Users\\Admin\\git\\repository\\crmZoho\\src\\test\\resources\\dataRepo\\config.properties";
+	String EXCEL_PATH="C:\\Users\\Admin\\git\\repository\\crmZoho\\src\\test\\resources\\dataRepo\\inputData.xlsx";
 	
 			
 
