71dd2a328bea4e62a96d1b4d78b30c32e964d744
Admin <Admin@DESKTOP-TAOG1EQ>
2022-07-05 14:11:59 +0530

    project commit

--- crmZoho/src/main/java/com/crm/Genriclib/MyListeners.java
+++ crmZoho/src/main/java/com/crm/Genriclib/MyListeners.java
@@ -5,66 +5,42 @@ import org.testng.ITestListener;
 import org.testng.ITestResult;
 import org.testng.Reporter;
 
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.ExtentTest;
-import com.aventstack.extentreports.markuputils.ExtentColor;
-import com.aventstack.extentreports.markuputils.MarkupHelper;
-import com.aventstack.extentreports.reporter.ExtentSparkReporter;
-import com.aventstack.extentreports.reporter.configuration.Theme;
-
-
 public class MyListeners implements ITestListener{
-	
-	ExtentSparkReporter spark;
-	ExtentReports extent;
-	ExtentTest test;
-
+	@Override
 	public void onTestStart(ITestResult result) {
-		//Reporter.log(result.getName()+"method started",true);
-		
-    	
-
+		Reporter.log(result.getName()+"method startrd",true);
 	}
 
+	@Override
 	public void onTestSuccess(ITestResult result) {
-		//Reporter.log(result.getName()+"method PASSED",true);
-		test=extent.createTest(result.getName());
-		test.pass(MarkupHelper.createLabel("PASS", ExtentColor.GREEN));
+		Reporter.log(result.getName()+"method PASSED",true);
 	}
 
+	@Override
 	public void onTestFailure(ITestResult result) {
-	   //Reporter.log(result.getName()+"method FAILED",true);
-		test=extent.createTest(result.getName());
-		test.fail(MarkupHelper.createLabel("FAIL", ExtentColor.RED));
+	   Reporter.log(result.getName()+"method FAILED",true);	
 	   WebDriverCommonLib wlib=new WebDriverCommonLib();
 	   wlib.getFullScreenshot("./screenshots/"+result.getName()+".png");
 	}
-     
-	
+
+	@Override
 	public void onTestSkipped(ITestResult result) {
 		Reporter.log(result.getName()+"method SKIPPED",true);
 	}
 
+	@Override
 	public void onStart(ITestContext context) {
-//	Reporter.log(context.getName()+"started",true);
-	spark=new ExtentSparkReporter("./CustomReports/myreport.html");
-  	spark.config().setTheme(Theme.DARK);
-  	spark.config().setDocumentTitle("crmZoho Report");
-  	spark.config().setReportName("Extent Reports 5");
-  	 extent =new ExtentReports();
-  	extent.attachReporter(spark);
-  	
-  	
+	  Reporter.log(context.getName()+"started",true);
 	}
 
-	
+	@Override
 	public void onFinish(ITestContext context) {
 		Reporter.log(context.getName()+"ended",true);
-		extent.flush();
-	}
-	
-	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
-		//TODO Auto-generated method stub 
 	}
 
+	
+	
+	
+	
 }
+ 
